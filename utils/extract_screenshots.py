5b855d8ef3b4449260f614ba85a069ca868cc069
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2023-09-06 22:18:50 +0200

    feat: small changes

--- utils/extract_screenshots.py
+++ utils/extract_screenshots.py
@@ -1,9 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import os
 import argparse
-from tqdm import tqdm
 
 from webgenerator.ScreenShutter import ScreenShutter
 
